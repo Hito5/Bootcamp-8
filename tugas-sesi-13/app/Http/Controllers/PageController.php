c14dec740311f08003fb53272542b7997d235621
Panca Ardhito <hitosanjaya@gmail.com>
2026-05-27 18:59:55 +0700

    Tugas Sesi 13

---
+++ tugas-sesi-13/app/Http/Controllers/PageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function products()
+    {
+        return view('products');
+    }
+
+    public function productDetail($id)
+    {
+        return view('product-detail', ['id' => $id]);
+    }
+
+    public function cart()
+    {
+        return view('cart');
+    }
+
+    public function checkout()
+    {
+        return view('checkout');
+    }
+}
